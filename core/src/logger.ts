b6e0eaf09d528e6c442bd991925396bd180d0e43
Mario Arnautou <mario.arnautou@gmail.com>
2021-07-27 17:17:28 +0200

    style: fix linter errors

--- core/src/logger.ts
+++ core/src/logger.ts
@@ -6,8 +6,8 @@ const print = (lvl: 'debug' | 'info' | 'warn' | 'error', ...args: unknown[]): vo
 }
 
 export const logger = {
-  debug: (...args: unknown[]) => print('debug', args),
-  info: (...args: unknown[]) => print('info', args),
-  warn: (...args: unknown[]) => print('warn', args),
-  error: (...args: unknown[]) => print('error', args),
+  debug: (...args: unknown[]): void => print('debug', args),
+  info: (...args: unknown[]): void => print('info', args),
+  warn: (...args: unknown[]): void => print('warn', args),
+  error: (...args: unknown[]): void => print('error', args),
 }
